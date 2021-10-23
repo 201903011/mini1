47615f99f4e964ed79149a0fea2ff44a9d10ec66
201903011 <72931833+201903011@users.noreply.github.com>
2021-10-23 15:43:39 +0530

    c

---
+++ src/main/java/com/mycompany/mini/Sett.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mini;
+
+/**
+ *
+ * @author user
+ */
+public class Sett {
+    
+    String s11;
+    
+    public void set( String s )
+    {
+        s11 = s ;
+    }
+    public String get()
+    {
+        return s11;
+    }
+    
+}
